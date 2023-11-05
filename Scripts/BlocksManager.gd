4ecc506a84c19f9c58c1167779b38656638b9da9
Benjamín Maldonado <benja.ignacio2001@gmail.com>
2023-11-05 15:59:20 -0300

    Se mueven los obstaculos

---
+++ Scripts/BlocksManager.gd
@@ -0,0 +1,16 @@
+extends Node
+
+@export var speed : float
+@export var bloks : Array[Node2D]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	bloks[0].position.x -= speed * delta
